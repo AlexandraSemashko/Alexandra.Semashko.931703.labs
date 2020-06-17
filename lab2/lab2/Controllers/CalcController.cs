048b7958a20d67bf6a918874a37b1dd6090b34cf
Александра Семашко <Alexandrasv18@mail.ru>
2020-06-17 22:40:49 +0700

    Initial commit

---
+++ lab2/lab2/Controllers/CalcController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using lab2.Models;
+
+namespace lab2.Controllers
+{ 
+    public class CalcController : Controller
+    { 
+        public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Manual(float x, float y, char operation)
+    {
+        if (Request.Method == "GET")
+        {
+            ViewData["Title"] = "Manual";
+            return View("Getall");
+        }
+        else
+        {
+            Calc cl = new Calc();
+            cl.x = x;
+            cl.y = y;
+            ViewBag.x = cl.x;
+            ViewBag.y = cl.y;
+            switch (operation)
+            {
+                case '+': ViewBag.res = cl.Add(); ViewBag.op = '+'; break;
+                case '-': ViewBag.res = cl.Sub(); ViewBag.op = '-'; break;
+                case '*': ViewBag.res = cl.Mult(); ViewBag.op = '*'; break;
+                case '/': ViewBag.res = cl.Div(); ViewBag.op = '/'; break;
+            }
+
+            ViewData["Title"] = "Result";
+            return View("Result1");
+        }
+
+    }
+
+
+    [HttpGet]
+    public IActionResult ManualWithSeparateHandlers()//get
+    {
+        ViewData["Title"] = "ManualWithSeparateHandlers";
+        return View("Getall");
+    }
+
+    [HttpPost]
+    public IActionResult ManualWithSeparateHandlers(float x, float y, char operation)//get
+    {
+        Calc cl = new Calc();
+        cl.x = x;
+        cl.y = y;
+        ViewBag.x = cl.x;
+        ViewBag.y = cl.y;
+        switch (operation)
+        {
+            case '+': ViewBag.res = cl.Add(); ViewBag.op = '+'; break;
+            case '-': ViewBag.res = cl.Sub(); ViewBag.op = '-'; break;
+            case '*': ViewBag.res = cl.Mult(); ViewBag.op = '*'; break;
+            case '/': ViewBag.res = cl.Div(); ViewBag.op = '/'; break;
+        }
+
+        ViewData["Title"] = "Result";
+        return View("Result1");
+    }
+
+    [HttpGet]
+    public IActionResult ModelBindingInParametrs()//get
+    {
+        ViewData["Title"] = "ModelBindingInParametrs";
+        return View("Getall");
+    }
+
+    [HttpPost]
+    public IActionResult ModelBindingInParametrs([Bind("x,y")]Calc cl, char operation)//get
+    {
+
+        switch (operation)
+        {
+            case '+': ViewBag.res = cl.Add(); break;
+            case '-': ViewBag.res = cl.Sub(); break;
+            case '*': ViewBag.res = cl.Mult(); break;
+            case '/': ViewBag.res = cl.Div(); break;
+        }
+
+        ViewData["Title"] = "Result";
+        return View("Result2");
+    }
+
+    [HttpGet]
+    public IActionResult ModelBindingInSeparateModel()//get
+    {
+        ViewData["Title"] = "ModelBindingInSeparateModel";
+        return View("Getall");
+    }
+
+    [HttpPost]
+    public IActionResult ModelBindingInSeparateModel(Calc cl, char operation)//get
+    {
+
+        switch (operation)
+        {
+            case '+': ViewBag.res = cl.Add(); break;
+            case '-': ViewBag.res = cl.Sub(); break;
+            case '*': ViewBag.res = cl.Mult(); break;
+            case '/': ViewBag.res = cl.Div(); break;
+        }
+
+        ViewData["Title"] = "Result";
+        return View("Result2");
+    }
+}
+
+}
---
+++ lab2/lab2/Models/Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab2.Models
+{
+    public class Calc
+    {
+        public float x { get; set; }
+        public float y { get; set; }
+
+        public Calc()
+        {
+            Random ran = new Random();
+            x = ran.Next(0, 10);
+            y = ran.Next(0, 10);
+        }
+
+        public float Add() { return x + y; }
+        public float Sub() { return x - y; }
+        public float Mult() { return x * y; }
+
+        public float Div()
+        {
+            if (y == 0) { return 0 ; }
+            else { return x / y; }
+        }
+    }
+}
